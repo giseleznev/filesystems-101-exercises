bbf29964609614820ced0e2fc2f57b795725dfe5
giseleznev <seleznev.gri@gmail.com>
2022-11-04 22:04:18 +0300

    try solution

--- 05-ext2-read-dir/solution.c
+++ 05-ext2-read-dir/solution.c
@@ -16,7 +16,7 @@ int report(int img, __le32 adr)
 
 	int size = 0;
 	struct ext2_dir_entry_2 *entry = (struct ext2_dir_entry_2 *) block;
-	while(size < data_size) {
+	while(size < block_size) {
 		char file_name[EXT2_NAME_LEN + 1];
 		memcpy(file_name, entry->name, entry->name_len);
 		file_name[entry->name_len] = '\0';
