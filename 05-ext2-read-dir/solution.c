dbd164710ea6ba1547fb045c789a165b1dadc489
giseleznev <seleznev.gri@gmail.com>
2022-11-05 20:33:53 +0300

    added single,double indirect

--- 05-ext2-read-dir/solution.c
+++ 05-ext2-read-dir/solution.c
@@ -6,8 +6,11 @@
 
 int block_size, data_size_left, error;
 
-int report(int img, __le32 adr)
+int report_direct(int img, __le32 adr)
 {
+	int length = data_size_left > block_size ? block_size : data_size_left;
+	data_size_left -= length;
+
 	void* block = malloc(block_size);
 	error = pread(img, block, block_size, block_size * adr);
 	if ( error < 0 ) {
@@ -18,19 +21,61 @@ int report(int img, __le32 adr)
 	int size = 0;
 	struct ext2_dir_entry_2 *entry = (void*) block;
 
-	//while(size < block_size && size < data_size_left && entry->inode) {
-		//entry = (void*) block + size;
+	while(size < length) {
+		entry = (void*) block + size;
+		if(entry->inode == 0) break;
 		size += entry->rec_len;
-		//if (size > block_size) break;
 		char file_name[EXT2_NAME_LEN + 1];
 		memcpy(file_name, entry->name, entry->name_len);
 		file_name[entry->name_len] = '\0';
 		char type = entry->file_type == 2 ? 'd' : 'f';
 		report_file(entry->inode , type, file_name);
-	//}
+	}
 
-	data_size_left -= size;
 	free(block);
+
+	return 0;
+}
+
+int report_indirect_single(int img, __le32 adr)
+{
+	// int as stored 4-byte block numbers
+	int* indirect_block = (int*)malloc(block_size);
+	error = pread(img, indirect_block, block_size, block_size * adr);
+	if ( error < 0 ) {
+		free(indirect_block);
+		return -errno;
+	}
+
+	// block_size / 4 - number of stored block numbers
+	for (int i = 0; i < (block_size / 4); i++) {
+		if (indirect_block[i] == 0) break;
+
+		report_direct(img, indirect_block[i]);
+	}
+
+	free(indirect_block);
+
+	return 0;
+}
+
+int report_indirect_double(int img, __le32 adr)
+{
+	int* double_indirect_block = (int*)malloc(block_size);
+	error = pread(img, double_indirect_block, block_size, block_size * adr);
+	if ( error < 0 ) {
+		free(double_indirect_block);
+		return -errno;
+	}
+
+	// block_size / 4 - number of stored block numbers
+	for (int i = 0; i < (block_size / 4); i++) {
+		if (double_indirect_block[i] == 0) break;
+
+		report_indirect_single(img, double_indirect_block[i]);
+	}
+
+	free(double_indirect_block);
 
 	return 0;
 }
@@ -58,12 +103,19 @@ int dump_dir(int img, int inode_nr)
 
 	data_size_left = inode.i_size;
 
-	//for(int i = 0; i < EXT2_NDIR_BLOCKS/*EXT2_N_BLOCKS*/; i++)
-	// if (S_ISDIR(inode.i_mode)) {
-	// 	if(inode.i_block[0/*i*/] != 0) {
-	// 		error = report(img, inode.i_block[0/*i*/]);
-	// 		if ( error < 0 ) return -errno;
-	// 	}
-	// }
+	for(int i = 0; i < EXT2_N_BLOCKS; i++)
+		if (i < EXT2_NDIR_BLOCKS) {                               	/* direct blocks */
+			error = report_direct(img, inode.i_block[i]);
+			if ( error < 0 ) return -errno;	
+		}
+		else if (i == EXT2_IND_BLOCK) {                         	/* single indirect block */
+			error = report_indirect_single(img, inode.i_block[i]);
+			if ( error < 0 ) return -errno;	
+		}
+		else if (i == EXT2_DIND_BLOCK) {                            /* double indirect block */
+			error = report_indirect_double(img, inode.i_block[i]);
+			if ( error < 0 ) return -errno;
+		}
+
 	return 0;
 }
